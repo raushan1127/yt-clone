7b39bc7c4bbac20779005c82203522d124ac67ff
raushan <kumarraushan2615@gmail.com>
2025-03-22 23:56:35 +0530

    ..

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from './Components/Head'
+
+const App = () => {
+  return (
+    <div className='bg-blue-50'>
+      <Head />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
