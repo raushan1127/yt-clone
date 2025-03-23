01f8ea005432befa6222b7b70b104b0a805d8a02
raushan <kumarraushan2615@gmail.com>
2025-03-23 23:17:30 +0530

    header-changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Head from './Components/Head'
+import Body from './Components/Body'
+
 
 const App = () => {
   return (
     <div className='bg-blue-50'>
       <Head />
       <Body />
-      <Footer />
     </div>
   )
 }
--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <div>
-        
+    <div className='grid grid-flow-col pl-3 shadow-lg py-2'>
+        <div className='flex col-span-1 p-2'>
+            <img className='w-9' src='https://static.vecteezy.com/system/resources/previews/002/292/406/large_2x/hamburger-menu-line-icon-free-vector.jpg'>
+            </img>
+            <img className='w-10 h-9 ml-8 ' src='https://pngimg.com/uploads/youtube/youtube_PNG102352.png'>
+            </img>
+        </div>
+        <div className='col-span-10 pl-74 '>
+            <input  type='text' placeholder='Search' className='bg-slate-100 rounded-l-full px-60 border-b-slate-500 border-1 p-2 '></input>
+            <button className='rounded-r-full border-1  p-2'>🔍</button>
+        </div>
+        <div className='pt-2'>
+            <img className='w-10 h-8' src='https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png'>
+            </img>
+        </div>
+
     </div>
   )
 }
